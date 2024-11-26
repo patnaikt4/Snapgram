5e9e872011006294606eedb19e7624ebe8e4ad20
Tejas <tejaspatnaik@college.harvard.edu>
2024-11-26 14:59:44 -0500

    Basic Structures of Social Media App
    Implemented Structure for Different Pages and the Basic Layout for the Site

---
+++ Snapgram/src/_auth/AuthLayout.tsx
@@ -0,0 +1,32 @@
+import { Outlet, Navigate } from 'react-router-dom';
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ): (
+        <>
+          <section className = "flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section> 
+
+            <img
+              src="/assets/images/side-img.svg"
+              alt="logo"
+              className="h-screen w-1/2 object-cover bg-no-repeat" // I had to remove hidden and x1:block because it wouldn't show up on my machine, But I ideally want it to be that way
+            />
+
+
+
+        </>
+
+      )}
+    </>
+  );
+}
+
+export default AuthLayout
