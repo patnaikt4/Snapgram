1d2cee2fcd5538c8bfeaa9c350cfde10a611bc95
Tejas <tejaspatnaik@college.harvard.edu>
2024-12-01 16:37:03 -0500

    Integration with AppWrite

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -17,13 +17,9 @@ const AuthLayout = () => {
             <img
               src="/assets/images/side-img.svg"
               alt="logo"
-              className="h-screen w-1/2 object-cover bg-no-repeat" // I had to remove hidden and x1:block because it wouldn't show up on my machine, But I ideally want it to be that way
+              className="hidden x1:block h-screen w-1/2 object-cover bg-no-repeat" // I had to remove hidden and x1:block because it wouldn't show up on my machine, But I ideally want it to be that way
             />
-
-
-
         </>
-
       )}
     </>
   );
